941abb52f41ebec8c42d778ad0dc775b0c5e568f
Kengkkun <waengchaikkun@gmail.com>
2021-04-27 18:42:42 +0700

    SYNC Invoice finish

--- src/pages/Overview/OverviewTable.tsx
+++ src/pages/Overview/OverviewTable.tsx
@@ -1,4 +1,4 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -14,35 +14,47 @@ const useStyles = makeStyles({
   },
 });
 
+const getMuiTheme = () => createMuiTheme({
+  overrides: {
+    MuiTableContainer: {
+      root: {
+        position: 'absolute'
+      }
+    },
+  }
+})
+
 export const OverviewTable = ({ rows }: any) => {
   const classes = useStyles();
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Building Name</TableCell>
-            <TableCell align="right">Building Rank</TableCell>
-            <TableCell align="right">Energy Intensity</TableCell>
-            <TableCell align="right">Energy(kWh)</TableCell>
-            <TableCell align="right">Energy Use Percent</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row: any) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.building_name}
-              </TableCell>
-              <TableCell align="right">{row.building_rank}</TableCell>
-              <TableCell align="right">{row.energy_intensity}</TableCell>
-              <TableCell align="right">{row.energy_kWh}</TableCell>
-              <TableCell align="right">{row.energy_use_percent}</TableCell>
+    <MuiThemeProvider theme={getMuiTheme()}>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Building Name</TableCell>
+              <TableCell align="right">Building Rank</TableCell>
+              <TableCell align="right">Energy Intensity</TableCell>
+              <TableCell align="right">Energy(kWh)</TableCell>
+              <TableCell align="right">Energy Use Percent</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row: any) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.building_name}
+                </TableCell>
+                <TableCell align="right">{row.building_rank}</TableCell>
+                <TableCell align="right">{row.energy_intensity}</TableCell>
+                <TableCell align="right">{row.energy_kWh}</TableCell>
+                <TableCell align="right">{row.energy_use_percent}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </MuiThemeProvider>
   );
 };
